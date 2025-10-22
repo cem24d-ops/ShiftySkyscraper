f71e1eaad76b2894ced7e5ddbe5bd066f715e2f2
Caden Martinez <cem24d@acu.edu>
2025-10-21 22:28:31 -0500

    New level for testing, along with two chars and 2 player movement scripts

---
+++ Shifty_Skyscraper_ITC110_Goated/Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class PlayerTestMovement : MonoBehaviour
+{
+    public KeyCode leftKey = KeyCode.A;
+    public KeyCode rightKey = KeyCode.D;
+    public KeyCode jumpKey = KeyCode.W;
+    public float moveSpeed = 10.0f;
+
+    public float jumpForce = 500.0f;
+
+    Rigidbody2D rb;
+
+    public bool isGrounded = false;
+
+    public bool shouldJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get horizontal input
+        if (Input.GetKey(leftKey))
+        {
+            rb.linearVelocity = new Vector2(-moveSpeed, rb.linearVelocity.y);
+        }
+        else if (Input.GetKey(rightKey))
+        {
+            rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+        }
+
+        // get jump input
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            shouldJump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (shouldJump == true)
+        {
+            // quickly set back to false so we don't double-jump
+            shouldJump = false;
+
+            //push the rigidbody UP
+            rb.AddForce(transform.up * jumpForce);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+            isGrounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+            isGrounded = false;
+    }
+}
