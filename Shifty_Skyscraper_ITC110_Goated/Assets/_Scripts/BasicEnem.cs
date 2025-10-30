a1441a8a7f6144f675562dfe37ec6ebf0b3388a4
Caden Martinez <cem24d@acu.edu>
2025-10-30 14:42:08 -0500

    Updated enemy detection!

--- Shifty_Skyscraper_ITC110_Goated/Assets/_Scripts/BasicEnem.cs
+++ Shifty_Skyscraper_ITC110_Goated/Assets/_Scripts/BasicEnem.cs
@@ -6,16 +6,45 @@ public class BasicEnem : MonoBehaviour
 {
 
     public float speed;
+    public bool isChasing = false;
     private Transform player;
+    private int playersInRange = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    /*
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
-
+    */
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the object entering the trigger is the player
+        if (other.CompareTag("Player"))
+        {
+            player = other.GetComponent<Transform>();
+            playersInRange++;
+            isChasing = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // Check if the object exiting the trigger is the player
+        if (other.CompareTag("Player"))
+        {
+            playersInRange--;
+            if (playersInRange <= 0)
+            {
+                isChasing = false;
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        // Move towards the player if chasing
+        if (player != null && isChasing)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
     }
 }
