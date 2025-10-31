825932e2a74a7f0ef23b62cdecfa4b92d10e8020
ImperialPlayz <jdh24e@acu.edu>
2025-10-30 22:34:51 -0500

    Level 2A (incompleter) and everything that came with it
    - Created animations folder
    - Added a Rubik's Cube as a demo object for a puzzle item
    - Added more to the InteractableTrigger script (more customizable)
    - Added TriggeredStageLayer2 and TriggeredSolidLayer1 (trust the process, it'll make sense by Monday)
    - Quick Note: hopefully once I get 2A down 2B and 2C should take a little less time since it's the same mechanics/puzzle types just with different level design

--- Shifty_Skyscraper_ITC110_Goated/Assets/_Scripts/InteractableTrigger.cs
+++ Shifty_Skyscraper_ITC110_Goated/Assets/_Scripts/InteractableTrigger.cs
@@ -2,17 +2,20 @@ using UnityEngine;
 
 public class InteractableTrigger : MonoBehaviour
 {
-    public GameObject enableObject;
-    public InteractableSpriteChanger Object1;
-    public InteractableSpriteChanger Object2;
-    public InteractableSpriteChanger Object3;
-    public InteractableSpriteChanger Object4;
-
+    // true will enable the object, false will disable the object (has to be disabled for true to work, and vice versa)
+    public bool statusChange = true; // can be changed in the inspector
+    public GameObject enableObject1, enableObject2;
+    public InteractableSpriteChanger Object1, Object2, Object3, Object4;
+    
     void Update()
     {
+        // haven't figured a way to do any combination without a lot of if statements, so repeat objects in the inspector so it will work properly
         if ( Object1.hasTriggered && Object2.hasTriggered && Object3.hasTriggered && Object4.hasTriggered )
         {
-            enableObject.SetActive(true);
+            if (enableObject1 != null)
+                enableObject1.SetActive(statusChange);
+            if (enableObject2 != null)
+                enableObject2.SetActive(statusChange);
         }
     }
 }
