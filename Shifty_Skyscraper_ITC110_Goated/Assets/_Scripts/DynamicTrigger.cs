29de1acec6bef215473730e1ef401f59ed556c44
ImperialPlayz <jdh24e@acu.edu>
2025-11-04 13:11:19 -0600

    Minor (But Important) Level Changes
    - Also testing a way to streamline a mechanic for Level 2A (DynamicTrigger)
      - It worked :)
      - Updated both levels to use DynamicTrigger instead of InteractableTrigger

---
+++ Shifty_Skyscraper_ITC110_Goated/Assets/_Scripts/DynamicTrigger.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+public class DynamicTrigger : MonoBehaviour
+{
+    // true will enable the object, false will disable the object (has to be disabled for true to work, and vice versa)
+    public bool statusChange = true; // can be changed in the inspector
+    public int objectAmount = 0;
+    public int triggerSize = 0;
+    public GameObject[] enableObject;
+    public InteractableSpriteChanger[] objectTrigger;
+
+    private int triggered = 0;
+
+    void Update()
+    {
+        /*
+            Looking to check each objectTrigger before enabling ALL objects
+            Use triggerSize to determine loop
+        */
+
+        for (int i = 0; i < triggerSize; i++)
+        {
+            if (objectTrigger[i].hasTriggered)
+            {
+                triggered++;
+            }
+            else
+            {
+                triggered = 0;
+                break;
+            }
+        }
+
+        if (triggered == triggerSize)
+        {
+            for (int i = 0; i < objectAmount; i++)
+            {
+                enableObject[i].SetActive(statusChange);
+            }
+        }
+    }
+}
