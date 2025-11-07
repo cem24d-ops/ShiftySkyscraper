6384f1ecaea35e1338416a8b11687fefd0fa93a3
ImperialPlayz <jdh24e@acu.edu>
2025-11-07 09:58:39 -0600

    New Mechanic (Again!)
    - Changed HoldTrigger to DualTrigger
    - Created HoldTrigger (again) to detect when a player is holding down the interact button to enable objects as long as it's being pressed down (not working yet, but i wanna get this out there)

--- Shifty_Skyscraper_ITC110_Goated/Assets/_Scripts/DualTrigger.cs
+++ Shifty_Skyscraper_ITC110_Goated/Assets/_Scripts/DualTrigger.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-public class HoldTrigger : MonoBehaviour
+public class DualTrigger : MonoBehaviour
 {
     // true will enable the object, false will disable the object (has to be disabled for true to work, and vice versa)
     public bool statusChange = true; // can be changed in the inspector
     public int objectAmount = 0;
     public GameObject[] enableObject;
-    public InteractableSpriteChanger objectTrigger;
+    public InteractableSpriteChanger objectTrigger1, objectTrigger2;
     public PlayerMovement player1, player2;
 
     void Start()
@@ -17,12 +17,13 @@ public class HoldTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            while (Input.GetKey(player1.interactKey) || Input.GetKey(player2.interactKey))
+            if (Input.GetKey(player1.interactKey) && Input.GetKey(player2.interactKey))
             {
                 for (int i = 0; i < objectAmount; i++)
                 {
                     enableObject[i].SetActive(statusChange);
                 }
+                Debug.Log("interact p1 + p2");
             }
         }
     }
